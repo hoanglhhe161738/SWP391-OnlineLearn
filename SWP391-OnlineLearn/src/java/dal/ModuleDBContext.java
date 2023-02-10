eced2b3e004f0a98fb71c7a146aaea94d1bf750e
Khangnekk <contact.khalgfk@gmail.com>
2023-02-10 22:30:20 +0700

    update back-end for course related pages

--- SWP391-OnlineLearn/src/java/dal/ModuleDBContext.java
+++ SWP391-OnlineLearn/src/java/dal/ModuleDBContext.java
@@ -35,8 +35,25 @@ public class ModuleDBContext extends DBContext<Module>{
     }
 
     @Override
-    public Module get(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Module get(int module_id) {
+        Module module = new Module();
+        CourseDBContext cDB = new CourseDBContext();
+        ArrayList<Course> courses = cDB.list();
+        String sql = "SELECT * FROM Module WHERE module_id = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, module_id);
+            ResultSet rs = stm.executeQuery();
+            if(rs.next()){
+                int course_id = rs.getInt("course_id");
+                module.setModule_id(rs.getInt("module_id"));
+                module.setModule_name(rs.getString("module_name"));
+                module.setCourse(courses.stream().filter(co->co.getCourse_id() == course_id).findAny().get());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ModuleDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return  module;
     }
 
     @Override
