c0e2b61b928895da35b10655c01af5fee7ab32bf
Khangnekk <contact.khalgfk@gmail.com>
2023-02-11 22:32:52 +0700

    (BE) processing (retrieving percentage of lessons learned)

--- SWP391-OnlineLearn/src/java/controller/course/lessionController.java
+++ SWP391-OnlineLearn/src/java/controller/course/lessionController.java
@@ -9,21 +9,40 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import model.Lession;
 
 /**
  *
  * @author Khangnekk
  */
-public class lessionController extends HttpServlet{
+public class lessionController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        // doPost
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        // doGet
     }
-    
+
+    protected String getPercentLession(ArrayList<Lession> lessions) {
+        String result;
+        int learnStatusTrue = 0,
+                learnStatusFalse = 0,
+                numberOfLessions = lessions.size();
+        double percent;
+        for (Lession l : lessions) {
+            if (l.isStatus()) {
+                learnStatusTrue++;
+            } else {
+                learnStatusFalse++;
+            }
+        }
+        percent = (learnStatusTrue * 100) / numberOfLessions;
+        result = percent + "%";
+        return result;
+    }
 }
