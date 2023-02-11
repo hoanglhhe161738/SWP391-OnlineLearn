229d482598424650655489da7d4e6220e7cc31b4
Khangnekk <contact.khalgfk@gmail.com>
2023-02-11 20:06:10 +0700

    security processor core (encode: md5,sha-1)

---
+++ SWP391-OnlineLearn/src/java/controller/auth/securityProcessorCore.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.auth;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Khangnekk
+ */
+public class securityProcessorCore {
+
+    public static String SHA1EncodePassword(String inputPassword) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        byte[] result = md.digest(inputPassword.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+    public static String md5EncodePassword(String inputPassword) throws NoSuchAlgorithmException {
+        String password = inputPassword.trim();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] messageDigest = md.digest(password.getBytes());
+        BigInteger no = new BigInteger(1, messageDigest);
+        String hashtext = no.toString(16);
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
+    }
+}
