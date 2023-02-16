36f72afa1444ca51560d591b75091bafb18ad4d6
KienKhongMuonCode <kienpthe161863@fpt.edu.vn>
2023-02-16 23:22:36 +0700

    code back-end login

--- SWP391-OnlineLearn/src/java/controller/auth/LoginController.java
+++ SWP391-OnlineLearn/src/java/controller/auth/LoginController.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 package controller.auth;
 
 import java.io.IOException;
@@ -11,22 +14,36 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+<<<<<<< Updated upstream
 import model.Account;
+=======
+import dal.*;
+import model.*;
+>>>>>>> Stashed changes
 
 /**
  *
  * @author T490
  */
 public class LoginController extends HttpServlet {
+<<<<<<< Updated upstream
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+=======
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+>>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+<<<<<<< Updated upstream
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -46,6 +63,17 @@ public class LoginController extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
+=======
+            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+>>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -53,12 +81,23 @@ public class LoginController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+<<<<<<< Updated upstream
     throws ServletException, IOException {
         processRequest(request, response);
     } 
 
     /** 
      * Handles the HTTP <code>POST</code> method.
+=======
+            throws ServletException, IOException {
+//        response.sendRedirect("./login.jsp");
+        request.getRequestDispatcher("./login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+>>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -66,12 +105,31 @@ public class LoginController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
+<<<<<<< Updated upstream
     throws ServletException, IOException {
         processRequest(request, response);
     }
 
     /** 
      * Returns a short description of the servlet.
+=======
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        AccountDBContext abd = new AccountDBContext();
+        Account a = abd.login(username, password);
+        if (a == null) {
+            request.getRequestDispatcher("./login.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("./welcome.html").forward(request, response);
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+>>>>>>> Stashed changes
      * @return a String containing servlet description
      */
     @Override
