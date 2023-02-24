22911e5104ec1621c1fdc3a747711dd1032459fa
KienKhongMuonCode <kienpthe161863@fpt.edu.vn>
2023-02-24 16:29:09 +0700

    remove membership in class Account.java and fix somethings p2

--- SWP391-OnlineLearn/src/java/dal/DBContext.java
+++ SWP391-OnlineLearn/src/java/dal/DBContext.java
@@ -25,9 +25,7 @@ public abstract class DBContext<T> {
             String url = "jdbc:sqlserver://localhost\\MSSQL:1433;databaseName=SWP391_Group2";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection =  DriverManager.getConnection(url,username,password);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }       
     }
