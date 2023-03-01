69fb77c38b5c87d9bd43f8f74b53ff44184b30ed
Khangnekk <contact.khalgfk@gmail.com>
2023-03-02 02:02:11 +0700

    Create changeUserProfileController.java

---
+++ SWP391-OnlineLearn/src/java/controller/profile/changeUserProfileController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.profile;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author Khangnekk
+ */
+public class changeUserProfileController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("./changeuserprofile.jsp").forward(req, resp);
+    }
+    
+}
