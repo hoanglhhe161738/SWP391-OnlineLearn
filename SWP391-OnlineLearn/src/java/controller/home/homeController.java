7e1b2e59301ac797b0b9f6d57367d8c1275b2fc9
Khangnekk <contact.khalgfk@gmail.com>
2023-03-06 23:11:09 +0700

    update home

--- SWP391-OnlineLearn/src/java/controller/home/homeController.java
+++ SWP391-OnlineLearn/src/java/controller/home/homeController.java
@@ -7,7 +7,6 @@ package controller.home;
 import controller.auth.BaseAuthenticationController;
 import dal.UserDBContext;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
