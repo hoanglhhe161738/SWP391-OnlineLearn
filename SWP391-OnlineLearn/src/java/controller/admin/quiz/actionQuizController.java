6d63f5de16e8e8e3f3ab1b662f1de6b8ff10df9a
Khangnekk <contact.khalgfk@gmail.com>
2023-03-08 23:51:03 +0700

    back-end add quiz

--- SWP391-OnlineLearn/src/java/controller/admin/quiz/actionQuizController.java
+++ SWP391-OnlineLearn/src/java/controller/admin/quiz/actionQuizController.java
@@ -4,41 +4,40 @@
  */
 package controller.admin.quiz;
 
-import dal.LessonDBContext;
 import dal.QuestionDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import model.Lesson;
 import model.Question;
 
 /**
  *
  * @author Khangnekk
  */
-public class AddQuizController extends HttpServlet{
+public class actionQuizController extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LessonDBContext lDB = new LessonDBContext();
-        int lesson_id = Integer.parseInt(req.getParameter("lession"));
-        Lesson lesson = lDB.get(lesson_id);
-        req.getSession().setAttribute("lesson", lesson);
-        req.getRequestDispatcher("./choiceQuizLession.jsp").forward(req, resp);
+        super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         QuestionDBContext qDB = new QuestionDBContext();
-        Lesson lesson = (Lesson) req.getSession().getAttribute("lesson");
-        ArrayList<Question> questions = qDB.getQuestionByLessionID(lesson.getLesson_id());
-        int questionSize = questions.size();
-        req.getSession().setAttribute("questions", questions);
-        req.getSession().setAttribute("questionSize", questionSize);
-        req.setAttribute("action", "-1");
+        String action = req.getParameter("action");
+        int index = Integer.parseInt(req.getParameter("index"));
+        int question_id = Integer.parseInt(req.getParameter("question_id"));
+        if(question_id!=-1){
+            Question question = qDB.get(question_id);
+            req.setAttribute("question", question);
+        }
+        req.setAttribute("action", action);
+        req.setAttribute("index", index);
+        req.setAttribute("add", "add");
+        req.setAttribute("edit", "edit");
+        req.setAttribute("delete", "delete");
         req.getRequestDispatcher("./addQuiz.jsp").forward(req, resp);
     }
     
