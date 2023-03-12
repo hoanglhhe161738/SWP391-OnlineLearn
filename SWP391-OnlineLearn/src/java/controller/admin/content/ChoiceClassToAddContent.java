13a0a94f76e63474db2a909a2abe2be98f0f9f76
hnahk02 <100854712+hnahk02@users.noreply.github.com>
2023-03-12 16:05:38 +0700

    Create ChoiceClassToAddContent.java

---
+++ SWP391-OnlineLearn/src/java/controller/admin/content/ChoiceClassToAddContent.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.admin.content;
+
+import dal.ClassDBContext;
+import dal.CourseDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Course;
+
+
+/**
+ *
+ * @author Acer
+ */
+public class ChoiceClassToAddContent extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int class_id = Integer.parseInt(req.getParameter("grade"));
+        CourseDBContext cDB = new CourseDBContext();
+        ArrayList<Course> courses = cDB.listCoursebyClassId(class_id);
+        req.getSession().setAttribute("class_id", class_id);
+        req.getSession().setAttribute("coursesToAddQuiz", courses);
+        req.getRequestDispatcher("./choiceContentLession.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ClassDBContext clDB = new ClassDBContext();
+        ArrayList<model.Class> classes = clDB.list();
+        req.getSession().setAttribute("classesToAddQuiz", classes);
+        req.getRequestDispatcher("./choiceContentLession.jsp").forward(req, resp);
+    }
+
+    
+    
+    
+}
