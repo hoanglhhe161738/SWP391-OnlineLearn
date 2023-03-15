a73ff78d360ddbed62aa8e8f7dd751708e09ec41
hnahk02 <100854712+hnahk02@users.noreply.github.com>
2023-03-16 01:13:30 +0700

    Update ModuleDBContext.java

--- SWP391-OnlineLearn/src/java/dal/ModuleDBContext.java
+++ SWP391-OnlineLearn/src/java/dal/ModuleDBContext.java
@@ -21,12 +21,43 @@ public class ModuleDBContext extends DBContext<Module> {
 
     @Override
     public void insert(Module model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "INSERT INTO [dbo].[Module]\n"
+                + "           ([module_name]\n"
+                + "           ,[course_id]\n"
+                + "           ,[status])\n"
+                + "     VALUES\n"
+                + "           (?\n"
+                + "           ,?\n"
+                + "           ,?)";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, model.getModule_name());
+            stm.setInt(2, model.getCourse().getCourse_id());
+            stm.setBoolean(3, model.isStatus());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ModuleDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void update(Module model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "UPDATE [dbo].[Module]\n"
+                + "   SET [module_name] = ?\n"
+                + "      ,[course_id] = ?\n"
+                + "      ,[status] = ?\n"
+                + " WHERE module_id = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, model.getModule_name());
+            stm.setInt(2, model.getCourse().getCourse_id());
+            stm.setBoolean(3, model.isStatus());
+            stm.setInt(4, model.getModule_id());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ModuleDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     @Override
